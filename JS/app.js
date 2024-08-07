4ebeef1109be845c745fbbe34ba44905c1ccec98
Chanukalankanjana <chanukalankanjana1119@gmail.com>
2024-08-08 00:06:50 +0530

    add income function

---
+++ JS/app.js
@@ -0,0 +1,57 @@
+document.getElementById('expense-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    addExpense();
+});
+
+document.getElementById('income-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    addIncome();
+});
+
+document.getElementById('clear-summary-btn').addEventListener('click', function() {
+    clearSummary();
+});
+
+let selectedCategory = 'Food';
+let editExpenseId = null;
+let editIncomeId = null;
+
+document.querySelectorAll('.category-item').forEach(btn => {
+    btn.addEventListener('click', function() {
+        document.querySelectorAll('.category-item').forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+        selectedCategory = this.dataset.category;
+    });
+});
+
+function addIncome() {
+    const description = document.getElementById('income-description').value;
+    const amount = document.getElementById('income-amount').value;
+    const date = document.getElementById('income-date').value;
+
+    const incomes = JSON.parse(localStorage.getItem('incomes')) || [];
+
+    if (editIncomeId) {
+        const index = incomes.findIndex(inc => inc.id === editIncomeId);
+        incomes[index] = {
+            description,
+            amount: parseFloat(amount).toFixed(2),
+            id: editIncomeId,
+            date
+        };
+        editIncomeId = null;
+    } else {
+        const income = {
+            description,
+            amount: parseFloat(amount).toFixed(2),
+            id: Date.now(),
+            date
+        };
+        incomes.push(income);
+    }
+
+    localStorage.setItem('incomes', JSON.stringify(incomes));
+    displayIncomes();
+    updateChart();
+    clearIncomeForm();
+}
